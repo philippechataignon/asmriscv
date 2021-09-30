48902fcc2907207df3e564b783e0563900e2306c
Username <user@example.com>
2021-09-30 19:47:35 +0000

    Version revue strcpy

--- strcpy.s
+++ strcpy.s
@@ -1,40 +1,38 @@
 # strcpy en assembleur Risc-V,d'après Patterson et Hennessy
-#
-
-    .global strcpy  # adresse de démarrage du programme pour 'editeur de liens
+    .global main
     .equ    write,64
     .equ    exit,93
 
     .text
 
-strcpy:
+main:
     li   t0,0          # i <- 0+0
-    la   x10,destination
-    la   x11,origine
-L1: add  x5,t0,x11     # adresse de y[i] dans x5
-    lbu  x6,0(x5)       # x6 <- y[i]
-    add  x7,t0,x10     # adresse de x[i] dans x7
-    sb   x6,0(x7)       # x[i] <- y[i]
-    beq  x6,x0,L2       # si caractère NULL,c’est fini
-    addi t0,x19,1      # sinon i <- i+1
-    j    L1             # on va en L1
-L2: lw   t0,0(sp)      # restauration de x19
-    addi sp,sp,4        # étête la pile d’un mot
-    addi a0,x0,1        # 1 = StdOut
-    la   a1,destination # charger l’adresse
-    li   a2,%lo(len)       # longueur de la chaîne
-    li   a7,write       # appel système Linux write
-    ecall               # appel Linux écriture de la chaîne
+    la   a2,origine
+    la   a3,destination
+L1: add  a0,t0,a2        # adresse de y[i] dans x5
+    add  a1,t0,a3        # adresse de x[i] dans x7
+    lbu  t1,(a0)        # x6 <- y[i]
+    sb   t1,(a1)        # x[i] <- y[i]
+    beqz t1,L2        # si caractère NULL,c’est fini
+    addi t0,t0,1        # sinon i <- i+1
+    j    L1              # on va en L1
+L2:
+    li   a0,1               # 1 = StdOut
+    la   a1,destination     # charger l’adresse
+    li   a2,%lo(len)        # longueur de la chaîne
+    li   a7,write           # appel système Linux write
+    ecall                   # appel Linux écriture de la chaîne
 
-    li   a0,0           # code de retour 0
-    li   a7,exit       # le code de commande 93 
-    ecall             # Appel Linux pour finir
+    li   a0,0               # code de retour 0
+    li   a7,exit            # le code de commande 93 
+    ecall                   # Appel Linux pour finir
 
     .data
-
-destination:
-    .string "destination\n"	
 
 origine:
     .string	"initiations\n"
     .equ    len,. - origine
+
+destination:
+    .string "destination\n"	
+
