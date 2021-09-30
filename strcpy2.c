6e81f714444f95ef37a651ac40d2a9fc48f8abdd
Username <user@example.com>
2021-09-30 20:59:36 +0000

    Version c et s de strcpy2

---
+++ strcpy2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+int asm_strlen(char*);
+int asm_print(char*);
+int asm_strcpy(char*, char*);
+
+int main()
+{
+    char* str = "Essai\n";
+    char* dest = (char*) malloc(256);
+    asm_strcpy(str, dest);
+    asm_print(dest);
+    free(dest);
+    return 0;
+}
